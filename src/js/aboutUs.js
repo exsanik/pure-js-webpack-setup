96b976943b8a4eb5d88cf95b0ddf7520db0ce0f5
Nikita Turkin <nexsanik@gmail.com>
2020-12-06 22:01:09 +0200

    [Feature] inital

---
+++ src/js/aboutUs.js
@@ -0,0 +1,5 @@
+import '../styles/vendor.css'
+import '../styles/style.css'
+
+document.querySelector('#aboutUs-link a').classList.add('disabled')
+document.querySelector('#aboutUs-link').classList.add('active')
---
+++ src/js/contacts.js
@@ -0,0 +1,5 @@
+import '../styles/vendor.css'
+import '../styles/style.css'
+
+document.querySelector('#contacts-link a').classList.add('disabled')
+document.querySelector('#contacts-link').classList.add('active')
