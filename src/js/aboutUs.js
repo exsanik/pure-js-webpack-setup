ec738255f7f2e93b0323cedc05c810860305f39d
Nikita Turkin <nexsanik@gmail.com>
2020-12-06 22:45:45 +0200

    [Feature] replacing html loader to use params

--- src/js/aboutUs.js
+++ src/js/aboutUs.js
@@ -1,5 +1,2 @@
 import '../styles/vendor.css'
 import '../styles/style.css'
-
-document.querySelector('#aboutUs-link a').classList.add('disabled')
-document.querySelector('#aboutUs-link').classList.add('active')
--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,5 +1,2 @@
 import '../styles/vendor.css'
 import '../styles/style.css'
-
-document.querySelector('#contacts-link a').classList.add('disabled')
-document.querySelector('#contacts-link').classList.add('active')
